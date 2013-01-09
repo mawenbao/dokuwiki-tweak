030977ddb47ecaaa46c6656c1dd55ef71205cf4b
wilbur ma <wilbur.ma@hotmail.com>
2013-01-09 15:16:03 +0800

    fix sidbar scroll css bug, add makefile for yui-compressor

--- dokuwiki_custom_footer.js
+++ dokuwiki_custom_footer.js
@@ -1,1 +1,33 @@
-/var/www/static/js/dokuwiki_custom_footer.js
+// load js function
+function doku_load_js(u) {
+    var d=document,
+    p=d.getElementsByTagName('HEAD')[0],
+    c=d.createElement('script');
+
+    c.type='text/javascript';
+    c.src=u;
+    p.appendChild(c);
+}
+
+// for sidebar scrool
+(function(){
+    var oDiv=document.getElementById("p-toc");
+    var H=0,iE6;
+    var Y=oDiv;
+    while(Y){H+=Y.offsetTop;Y=Y.offsetParent};
+    iE6=window.ActiveXObject&&!window.XMLHttpRequest;
+    if(!iE6){
+        window.onscroll=function() {
+            var s=document.documentElement.scrollTop || document.body.scrollTop;
+            if (s>H) {
+                oDiv.className="portal sidebar-fixed";
+                if (iE6) {
+                    oDiv.style.top=(s-H)+"px";
+                }
+            } else {
+                oDiv.className="portal sidebar-absolute";
+            }    
+        };
+}
+})();
+
--- yui-compress.makefile
+++ yui-compress.makefile
@@ -1,1 +1,28 @@
-/root/scripts/yui-compress.makefile
+# Makefile for markdown conversion
+
+JAVA = java
+JAVA_FLAGS = -jar
+YUI_JAR = /usr/local/lib/openjdk-6/yuicompressor-2.4.7.jar
+YUI_FLAGS =
+SOURCES_JS := $(wildcard *.js)
+SOURCES_CSS = $(wildcard *.css)
+SOURCES = $(SOURCES_JS) $(SOURCES_CSS)
+OBJECTS_JS = $(patsubst %.js, %.min.js, $(SOURCES_JS))
+OBJECTS_CSS = $(patsubst %.css, %.min.css, $(SOURCES_CSS))
+OBJECTS = *.min.js *.min.css
+
+all: js css
+
+js: $(OBJECTS_JS)
+
+css: $(OBJECTS_CSS)
+
+$(OBJECTS_JS): %.min.js: %.js
+	$(JAVA) $(JAVA_FLAGS) $(YUI_JAR) $(YUI_FLAGS) -o $@ $<
+
+$(OBJECTS_CSS): %.min.css: %.css
+	$(JAVA) $(JAVA_FLAGS) $(YUI_JAR) $(YUI_FLAGS) -o $@ $<
+
+.PHONY: clean
+clean:
+	rm -f $(OBJECTS)
